9491abdb7ee792bba0238d5a3cb74cc839aec802
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-02-13 00:47:11 -0800

    more urls

--- url_category.py
+++ url_category.py
@@ -24,28 +24,26 @@ def always_whitelisted(url: str):
 
 def too_boring_to_log(url: str):
     if url in {
-            "https://docs.google.com/document/u/0/preload",
-        "https://app.slack.com/boot/block-kit-builder.html",
-        "https://app.slack.com/boot/client.html",
-        "https://app.slack.com/boot/docs.html",
-        "https://app.slack.com/boot/workflow-builder.html",
-        "https://beacons.gvt2.com/domainreliability/upload-nel",
-        "https://beacons2.gvt2.com/domainreliability/upload-nel",
-        "https://beacons4.gvt2.com/domainreliability/upload-nel",
-        "https://mail.google.com/domainreliability/upload",
         'http://www.google.com/gen_204',
         'https://api.rescuetime.com/collect',
+        "https://docs.google.com/document/u/0/preload",
+        "https://mail.google.com/domainreliability/upload",
     }:
         return True
     if url.startswith((
+        "https://app.slack.com/boot/",
+        'https://cello.client-channel.google.com/',
         'https://clients4.google.com/invalidation/lcs/client',
         'https://lh3.google.com/u/0/d/',
         'https://www.youtube.com/api/stats/atr',
         'https://www.youtube.com/yts/',
-
     )):
         return True
-    for substr in ['/generate_204', '/favicon.ico']:
+    for substr in [
+        '/generate_204',
+        '/favicon.ico',
+        '/domainreliability/upload-nel'
+    ]:
         if substr in url:
             return True
     return False
